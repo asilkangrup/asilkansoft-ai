55a31afd45fae67d9864b7b4179817f99119f425
asilkangrup <asilkangrup@gmail.com>
2026-09-01 13:23:34 +0300

    Move real estate profile engine to fresh account

--- app/Services/RealEstateProfileService.php
+++ app/Services/RealEstateProfileService.php
@@ -12,7 +12,7 @@ use Throwable;
 
 class RealEstateProfileService
 {
-    private const PRIMARY_USER_ID = 1;
+    private const PRIMARY_USER_ID = 40;
 
     public function process(
         ConversationControl $conversation,
