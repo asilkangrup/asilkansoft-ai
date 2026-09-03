25cac2ed146af4866d051d49d7318d6921dd0355
asilkangrup <asilkangrup@gmail.com>
2026-09-03 12:52:37 +0300

    Configure Satılık mı publishing bridge

--- config/services.php
+++ config/services.php
@@ -9,8 +9,8 @@ return [
     |
     | This file is for storing the credentials for third party services such
     | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
+    | location for this type of information, allowing packages to have a
+    | conventional file to locate the various service credentials.
     |
     */
 
@@ -33,6 +33,12 @@ return [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
+    ],
+
+    'satilikmi' => [
+        'api_url' => env('SATILIKMI_API_URL', 'https://api.satilikmi.com/api/v1'),
+        'integration_key' => env('SATILIKMI_INTEGRATION_KEY'),
+        'public_url' => env('SATILIKMI_PUBLIC_URL', 'https://satilikmi.com'),
     ],
 
 ];
