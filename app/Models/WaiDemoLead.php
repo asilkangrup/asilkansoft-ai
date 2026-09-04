9579ad73f8e5113c99e4665a92d35f233dfef82b
asilkangrup <asilkangrup@gmail.com>
2026-09-05 02:04:27 +0300

    Relate demo leads to chat messages

--- app/Models/WaiDemoLead.php
+++ app/Models/WaiDemoLead.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class WaiDemoLead extends Model
 {
@@ -32,5 +33,10 @@ class WaiDemoLead extends Model
     public function temporaryBot(): BelongsTo
     {
         return $this->belongsTo(AiBot::class, 'temporary_bot_id');
+    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(WaiDemoMessage::class, 'wai_demo_lead_id')->orderBy('id');
     }
 }
