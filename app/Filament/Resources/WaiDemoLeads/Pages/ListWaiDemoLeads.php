70defc787f7a11cf625c9ac0e8e2826bab19a34f
asilkangrup <asilkangrup@gmail.com>
2026-09-05 00:45:25 +0300

    Add WAI demo tracking list page

---
+++ app/Filament/Resources/WaiDemoLeads/Pages/ListWaiDemoLeads.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\WaiDemoLeads\Pages;
+
+use App\Filament\Resources\WaiDemoLeads\WaiDemoLeadResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListWaiDemoLeads extends ListRecords
+{
+    protected static string $resource = WaiDemoLeadResource::class;
+}
