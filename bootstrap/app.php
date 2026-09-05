4c706f1dda479155d88434f7df676a6897c38e69
asilkangrup <asilkangrup@gmail.com>
2026-09-05 20:54:01 +0300

    Register isolated WAI sales webhook routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(
     basePath: dirname(__DIR__)
@@ -13,6 +14,11 @@ return Application::configure(
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function (): void {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/wai-sales.php'));
+        },
     )
 
     ->withMiddleware(function (Middleware $middleware): void {
