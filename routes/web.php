67c768512a8e9e997b996b5e4621cc1b3ce4ffff
asilkangrup <asilkangrup@gmail.com>
2026-09-07 20:53:18 +0300

    Add public insurance demo route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::view('/sigorta-demo', 'insurance-demo')
     ->middleware('throttle:120,1')
     ->name('insurance.demo');
 
+Route::view('/sigorta', 'insurance-public-demo')
+    ->middleware('throttle:120,1')
+    ->name('insurance.public.demo');
+
 Route::get('/demo/lead/{token}', [WaiLeadDemoController::class, 'show'])
     ->where('token', '[A-Za-z0-9]{48}')
     ->middleware('throttle:60,1')
