ebeb90f178f18cd39380aaa0dc13174b5b57eef9
asilkangrup <asilkangrup@gmail.com>
2026-09-14 14:24:33 +0300

    Register isolated Textile V2 provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\TextilePilotRuntimePatchProvider::class,
+    App\Providers\TextileV2ServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
 ];
