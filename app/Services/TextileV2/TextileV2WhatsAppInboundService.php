9586db23fbd33c6130569b1f5421e6cc81efd110
asilkangrup <asilkangrup@gmail.com>
2026-09-14 14:07:05 +0300

    Add isolated Istanbul Tisort V2 service skeleton

---
+++ app/Services/TextileV2/TextileV2WhatsAppInboundService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services\TextileV2;
+
+class TextileV2WhatsAppInboundService
+{
+    public const BOT_ID = 55;
+    public const USER_ID = 48;
+    public const INSTANCE = 'istanbul-tisort-v2-55';
+}
